c72790564295b42186f745c6503f0904cdb478c8
Jessica <jessica.zager@vml.com>
2026-05-27 15:06:07 -0500

    Wrote tests for locale switching persistence

---
+++ tests/fr.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+
+test('Locale persistence, FR', async ({page}) => {
+    await page.goto("/");
+    await page.getByRole("button", { name: "Accept Cookies" }).click();
+    await page.getByRole("button", { name: "United States (EN)" }).click();
+    await page.getByRole("menuitem", { name: "France (FR)" }).click();
+
+    await page.getByRole("link", { name: "Paradox Logo" }).click();
+    await expect(page).toHaveURL('/fr');
+
+    await page
+      .getByRole("link", { name: "Paradox of Plastic" })
+      .first()
+      .click();
+    await expect(page).toHaveURL("/fr/paradox-of-plastic");  
+
+    await page.getByRole("link", { name: "Impact Stories" }).first().click();
+    await expect(page).toHaveURL("/fr/stories");
+
+    await page.getByRole("link", { name: "Meet Zuzu" }).first().click();
+    await expect(page).toHaveURL("/fr/zuzu");
+
+    //Footer Links
+    await page.getByRole('link', { name: 'Paradox of Plastic', description: 'Learn more about our story', exact: true }).click();
+    await expect(page).toHaveURL("/fr/paradox-of-plastic");  
+    
+    await page.getByRole('link', { name: 'Impact Stories', description: 'Impact Stories', exact: true }).click();
+    await expect(page).toHaveURL("/fr/stories");
+    await page
+      .getByRole("link", {
+        name: "Meet Zuzu",
+        description: "Meet Zuzu",
+        exact: true,
+      })
+      .click();
+      await expect(page).toHaveURL("/fr/zuzu");
+})
