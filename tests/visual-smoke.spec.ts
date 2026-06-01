f248aba3bd1b18bae04ed8cb3a9d9247b7649d56
Jessica <jessica.zager@vml.com>
2026-06-01 12:34:38 -0500

    Visual reorganizing & troubleshooting

--- tests/visual-smoke.spec.ts
+++ tests/visual-smoke.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from "@playwright/test";
-import { paths } from "../utils/paths";
-import { forceFonts } from "../utils/helpers";
+import { paths } from "../utils/index";
+import { forceFonts } from "../utils/index";
+import { emulateLazyLoadScroll } from "../utils/index"
+import { emulateLazyLoadScrollV2 } from "../utils/index";
+import path from "path";
 
 for (const [pageName, { path, title }] of Object.entries(paths)) {
   test.describe(`${pageName} - Visual Regression `, () => {
@@ -8,14 +11,17 @@ for (const [pageName, { path, title }] of Object.entries(paths)) {
       tag: '@smoke'
     }, async ({ page, baseURL }) => {
       await page.goto(baseURL + path);
+      await page.waitForLoadState("domcontentloaded");
 
       await forceFonts(page);
+      await emulateLazyLoadScroll(page);
       await expect(page).toHaveScreenshot(`${pageName}-full.png`, {
         fullPage: true,
         animations: "disabled",
         maxDiffPixelRatio: 0.3,
         maxDiffPixels: 100,
-        threshold: 0.3
+        threshold: 0.3,
+        //stylePath: path.join(__dirname, "screenshot.css"),
       });
     });
     
---
+++ utils/index.ts
@@ -0,0 +1,7 @@
+export { paths } from "./paths";
+export { forceFonts } from "./helpers";
+export { emulateLazyLoadScrollV2 } from "./visual/helpers";
+export { emulateLazyLoadScroll } from "./visual/helpers2point0";
+export { components } from "./components";
+
+//No need to include Storybook helpers
