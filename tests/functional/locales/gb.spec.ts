bf9ec890b0813cf746e2194e8dccce81a637564e
Jessica <jessica.zager@vml.com>
2026-06-03 14:02:19 -0500

    Even though it's not visual, adding tags to update a sub-readme

--- tests/functional/locales/gb.spec.ts
+++ tests/functional/locales/gb.spec.ts
@@ -1,19 +1,22 @@
 import { test, expect } from "@playwright/test";
 
-test('Locale persistence, GB', async ({page}) => {
+test(
+  "Locale persistence, GB",
+  { tag: ["@locales", "@regression", "@gb"] },
+  async ({ page }) => {
     await page.goto("/");
     await page.getByRole("button", { name: "Accept Cookies" }).click();
     await page.getByRole("button", { name: "United States (EN)" }).click();
     await page.getByRole("menuitem", { name: "Great Britain (EN)" }).click();
 
     await page.getByRole("link", { name: "Paradox Logo" }).click();
-    await expect(page).toHaveURL('/gb');
+    await expect(page).toHaveURL("/gb");
 
     await page
       .getByRole("link", { name: "Paradox of Plastic" })
       .first()
       .click();
-    await expect(page).toHaveURL("/gb/paradox-of-plastic");  
+    await expect(page).toHaveURL("/gb/paradox-of-plastic");
 
     await page.getByRole("link", { name: "Impact Stories" }).first().click();
     await expect(page).toHaveURL("/gb/stories");
@@ -22,10 +25,22 @@ test('Locale persistence, GB', async ({page}) => {
     await expect(page).toHaveURL("/gb/zuzu");
 
     //Footer Links
-    await page.getByRole('link', { name: 'Paradox of Plastic', description: 'Learn more about our story', exact: true }).click();
-    await expect(page).toHaveURL("/gb/paradox-of-plastic");  
-    
-    await page.getByRole('link', { name: 'Impact Stories', description: 'Impact Stories', exact: true }).click();
+    await page
+      .getByRole("link", {
+        name: "Paradox of Plastic",
+        description: "Learn more about our story",
+        exact: true,
+      })
+      .click();
+    await expect(page).toHaveURL("/gb/paradox-of-plastic");
+
+    await page
+      .getByRole("link", {
+        name: "Impact Stories",
+        description: "Impact Stories",
+        exact: true,
+      })
+      .click();
     await expect(page).toHaveURL("/gb/stories");
     await page
       .getByRole("link", {
@@ -34,5 +49,6 @@ test('Locale persistence, GB', async ({page}) => {
         exact: true,
       })
       .click();
-      await expect(page).toHaveURL("/gb/zuzu");
-})
+    await expect(page).toHaveURL("/gb/zuzu");
+  },
+);
